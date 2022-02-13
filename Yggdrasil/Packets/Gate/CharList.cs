6980a3da04892b70d3f7719f304c1bd965e9420c
Djnandinho <69001346+djnandinho26@users.noreply.github.com>
2022-02-12 21:36:05 -0300

    dd

--- Yggdrasil/Packets/Gate/CharList.cs
+++ Yggdrasil/Packets/Gate/CharList.cs
@@ -18,22 +18,18 @@ namespace Yggdrasil.Packets.Game
                 packet.WriteInt((int)Tamer.Model);
                 packet.WriteByte((byte)Tamer.Level);
                 packet.WriteString(Tamer.Name);
-                for (int i = 0; i < 14; i++)
+                for (int i = 0; i < 13; i++)
                 {
                     Item item = Tamer.Equipment[i];
                     packet.WriteBytes(item.ToArray());
                 }
-                packet.WriteInt(Tamer.Partner.Species);
-                packet.WriteByte((byte)Tamer.Partner.Level);
-                packet.WriteString(Tamer.Partner.Name);
-                packet.WriteByte(1);
-                packet.WriteByte(0);
-                packet.WriteByte(0);
-                packet.WriteByte(0);
-                packet.WriteByte(0);
-                packet.WriteByte(0);
-                packet.WriteByte(0);
-                packet.WriteByte(0);
+                packet.WriteBytes(Tamer.ChipSets.ToArray());
+                packet.WriteBytes(Tamer.Equipment[14].ToArray());
+                packet.WriteBytes(new byte[68 * 5]);
+                packet.WriteBytes(new byte[768]);
+                packet.WriteBytes(new byte[140]);
+                packet.WriteInt(Tamer.Incubator);
+
             }
             packet.WriteByte(99);
         }
